8b532ec047d0aca320a105023fcfff5ac5f9d288
oscar avila <oscaravilaperilla@hotmail.com>
2018-09-03 19:24:46 -0500

    courses list

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,23 @@
 import * as types from './actionTypes';
-export function createCourse(course){
-    return {type: types.CREATE_COURSE, course};
+import 'babel-polyfill';
+import courseApi from '../api/mockCourseApi';
+
+export function loadCoursesSuccess(courses){
+    return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+export   function  loadCourses(){
+    // let courses;
+    // try{
+    //     courses = await courseApi.getAllCourses();
+    // } catch (e){
+    //     throw(error);
+    // }
+    return function(dispatch) {
+        return courseApi.getAllCourses().then(courses => {
+            dispatch(loadCoursesSuccess(courses));
+        }).catch(error => {
+            throw(error);
+        });
+    };
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,14 @@
 import {createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function cofigureStore(initialState){
 
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     return  createStore(rootReducer, initialState, composeEnhancers(
+
     
-    
-        applyMiddleware(reduxImmutableStateInvariant())
+        applyMiddleware(thunk, reduxImmutableStateInvariant())
     ));
 }
